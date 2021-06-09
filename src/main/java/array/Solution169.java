8c7d67c1760bc0850549b5d54d6e9d42b13b3f6c
Lux <929822286@qq.com>
2021-06-09 18:26:56 +0800

    Array

---
+++ src/main/java/array/Solution169.java
@@ -0,0 +1,48 @@
+package array;
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[3,2,3]
+//输出：3 
+//
+// 示例 2： 
+//
+// 
+//输入：[2,2,1,1,1,2,2]
+//输出：2
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。 
+// 
+// Related Topics 位运算 数组 分治算法
+
+
+import java.util.HashMap;
+
+class Solution169 {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            if (map.get(num) > nums.length / 2) {
+                return num;
+            }
+        }
+        throw new IllegalArgumentException("no this  number");
+    }
+}
