8cbba397e10d81d06f7831bfd6d7e57778d1cd62
Lux <929822286@qq.com>
2021-06-10 09:25:35 +0800

    Array

---
+++ src/main/java/array/Solution217.java
@@ -0,0 +1,53 @@
+package array;
+//给定一个整数数组，判断是否存在重复元素。
+//
+// 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [1,2,3,1]
+//输出: true 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,2,3,4]
+//输出: false 
+//
+// 示例 3: 
+//
+// 
+//输入: [1,1,1,3,3,4,3,2,4,2]
+//输出: true 
+// Related Topics 数组 哈希表
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
