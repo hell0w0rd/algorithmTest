9294c39ff6a41b6a72e77f771e42399dbdb4597e
Lux <929822286@qq.com>
2021-06-15 14:31:28 +0800

    fix:Array

--- src/main/java/array/Solution643.java
+++ src/main/java/array/Solution643.java
@@ -24,12 +24,12 @@ package array;
 
 class Solution643 {
     public double findMaxAverage(int[] nums, int k) {
-        int max = Integer.MIN_VALUE;
         int sum = 0;
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
-        for (int i = k; i < nums.length - 1; i++) {
+        int max = sum;
+        for (int i = k; i < nums.length; i++) {
             sum = sum - nums[i-k] + nums[i];
             max = Math.max(max,sum);
         }
